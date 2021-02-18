dff0d72663d29a2b5946bbca427f145be1963585
GaneshGS <gane.eflu@gmail.com>
2021-02-17 19:56:19 -0500

    added voice_navigation tool

--- title_cluster_app.py
+++ title_cluster_app.py
@@ -4,9 +4,9 @@ import en_core_web_sm
 import pickle as pkl
 
 app = Flask(__name__)
-title_model = pkl.load(open('kmeans.pkl', 'rb'))
-nlp = spacy.load('en_core_web_sm')
-
+#title_model = pkl.load(open('kmeans.pkl', 'rb'))
+#nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('voice_navigation')
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -14,10 +14,15 @@ def home():
 @app.route('/predict',methods=['POST'])
 
 def predict():#For rendering results on HTML GUI
+    output = []
     title = request.form["Title"]
     vector_emb = nlp(str(title))
-    prediction = title_model.predict([vector_emb.vector])
-    return render_template('index.html', title=title, Cluster_label=prediction[0])
+    for ent in vector_emb.ents:
+        output.append(ent.text)
+        output.append(ent.label_)
+    prediction = output
+    #prediction = title_model.predict([vector_emb.vector])
+    return render_template('index.html', title=title, Cluster_label=prediction)
 
 
 if __name__ == "__main__":
